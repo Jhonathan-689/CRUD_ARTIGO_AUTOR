e951f6cd1ec3aae2f3f156001eb63329eb5c3034
jhonathan-689 <jhonathannauralves@gmail.com>
2025-02-27 21:27:22 -0300

    pequenos ajustes.

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+session_start();
 
+// Inclui a conexão com o banco de dados
+require_once 'config/db_connect.php';
+
+// Redireciona para a página de login por padrão
+header("Location: views/login.php");
+exit;
 ?>
